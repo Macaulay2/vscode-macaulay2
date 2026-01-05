7cbd2cf59e8432fcbbf50d89861318a69b4a7510
Doug Torrance <dtorrance@piedmont.edu>
2026-01-04 20:04:05 -0500

    Add support for Macaulay2 code blocks in markdown previews

--- src/backend/extension.ts
+++ src/backend/extension.ts
@@ -5,6 +5,10 @@
 import * as vscode from "vscode";
 import * as completions from "./completionProviders";
 import * as repl from "./repl";
+import hljs from "highlight.js/lib/core";
+import hljsM2 from "highlightjs-macaulay2";
+
+hljs.registerLanguage("macaulay2", hljsM2);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,6 +19,24 @@ export function activate(context: vscode.ExtensionContext) {
 
   completions.activate(context);
   repl.activate(context);
+
+  return {
+    // markdown-it plugin to highlight m2 code in markdown previews
+    extendMarkdownIt(md: any) {
+      const highlight = md.options.highlight;
+      md.options.highlight = (code, lang) => {
+        if (lang && ["m2", "macaulay2"].includes(lang.toLowerCase())) {
+          return hljs.highlight(code, {
+            language: lang,
+            ignoreIllegals: true,
+          }).value;
+        } else {
+          return highlight(code, lang);
+        }
+      };
+      return md;
+    },
+  };
 }
 
 // this method is called when your extension is deactivated
